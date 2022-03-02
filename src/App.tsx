afdde4a41dbe1a84071d891ac2d4324f4e7ea6cc
Maximilian Richter <richter.maximilian@gmx.net>
2022-03-02 17:42:28 +0100

    add github corner banner

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,17 @@ import "./App.css";
 import { Game } from "./components/Game/Game";
 import boards from "./boards";
 import { getRandomIndex } from "./utils";
+import GithubCorner from "react-github-corner";
 
 function App() {
   const boardsIdx = getRandomIndex(boards.length);
   return (
     <div className="App">
+      <GithubCorner
+        href="https://github.com/richterm/maxjongg"
+        bannerColor="#2b223a"
+        octoColor="#03edf9"
+      />
       <Game boardDefinition={boards[boardsIdx]} />
     </div>
   );

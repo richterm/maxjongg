9ea0e5ec3a60361038d70bdf74c2c28fc8a94874
Maximilian Richter <richter.maximilian@gmx.net>
2022-08-08 16:38:57 +0200

    remove measure

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -5,7 +5,6 @@ import "./Board.css";
 import { Tile } from "../Tile/Tile";
 import type { BoardState, TileType } from "../../types";
 import { areEqualTiles, isTileHinted } from "../../tiles";
-import Measure from "react-measure";
 
 type OnTileClickType = (tile: TileType) => void;
 
@@ -25,45 +24,40 @@ export const Board: FC<BoardProps> = ({
   style,
 }) => {
   return (
-    <Measure bounds>
-      {({ measureRef, contentRect: { bounds } }) => (
-        <div
-          ref={measureRef}
-          className="board"
-          style={
-            {
-              ...style,
-              "--red": "#ff7ebd",
-              "--green": "#72f1b8",
-              "--orange": "#fede5d",
-              "--blue": "#03edf9",
-              "--tile-color-hue": 263,
-              "--tile-color-saturation": 27,
-              "--tile-color-lightness": 18,
-              "--tile-shadow-color": "rgba(255,255,255,.25)",
-              "--tile-width": (bounds?.width ?? 0) / columns,
-              "--board-columns": columns,
-              "--board-rows": rows,
-            } as Properties
-          }
-        >
-          <NaturalShadowFilter />
-          {boardState.tiles.map((tile) => {
-            return (
-              <Tile
-                {...tile}
-                key={`${tile.x}_${tile.y}_${tile.z}_${tile.value}`}
-                selected={
-                  boardState.selected !== undefined &&
-                  areEqualTiles(boardState.selected, tile)
-                }
-                hinted={isTileHinted(boardState, tile)}
-                onClick={() => onTileClick(tile)}
-              />
-            );
-          })}
-        </div>
-      )}
-    </Measure>
+    <div
+      className="board"
+      style={
+        {
+          ...style,
+          "--red": "#ff7ebd",
+          "--green": "#72f1b8",
+          "--orange": "#fede5d",
+          "--blue": "#03edf9",
+          "--tile-color-hue": 263,
+          "--tile-color-saturation": 27,
+          "--tile-color-lightness": 18,
+          "--tile-shadow-color": "rgba(255,255,255,.25)",
+          "--board-columns": columns,
+          "--board-rows": rows,
+          "--tile-width-base": 4,
+        } as Properties
+      }
+    >
+      <NaturalShadowFilter />
+      {boardState.tiles.map((tile) => {
+        return (
+          <Tile
+            {...tile}
+            key={`${tile.x}_${tile.y}_${tile.z}_${tile.value}`}
+            selected={
+              boardState.selected !== undefined &&
+              areEqualTiles(boardState.selected, tile)
+            }
+            hinted={isTileHinted(boardState, tile)}
+            onClick={() => onTileClick(tile)}
+          />
+        );
+      })}
+    </div>
   );
 };

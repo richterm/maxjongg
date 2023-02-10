4dd4de6f382c27a51c21df84139cc78209b28c3f
Maximilian Richter <richter.maximilian@gmx.net>
2023-02-10 18:05:10 +0100

    add bounds wrapper

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 import { Canvas } from "@react-three/fiber";
-import { CameraControls } from "@react-three/drei/core";
+import { Bounds, CameraControls, OrbitControls } from "@react-three/drei/core";
 import { Tile } from "../Tile/Tile";
 import type { BoardState, TileType } from "../../types";
 import { areEqualTiles, isTileHinted } from "../../tiles";
@@ -23,30 +23,28 @@ export const Board: FC<BoardProps> = ({
   return (
     <Canvas
       camera={{
-        fov: 45,
-        position: [columns / -10, rows / 5, 40],
+        fov: 25,
+        position: [0, 0, 40],
       }}
     >
-      <CameraControls smoothTime={0.2} />
-      <pointLight
-        position={[0, 0, 100]}
-        intensity={1}
-        color={"white"}
-      />
-      {boardState.tiles.map((tile) => {
-        return (
-          <Tile
-            {...tile}
-            key={`${tile.x}_${tile.y}_${tile.z}_${tile.value}`}
-            selected={
-              boardState.selected !== undefined &&
-              areEqualTiles(boardState.selected, tile)
-            }
-            hinted={isTileHinted(boardState, tile)}
-            onClick={() => onTileClick(tile)}
-          />
-        );
-      })}
+      <OrbitControls makeDefault dampingFactor={0.1} />
+      <pointLight position={[0, 0, 100]} intensity={1} color={"white"} />
+      <Bounds fit observe margin={1} clip>
+        {boardState.tiles.map((tile) => {
+          return (
+            <Tile
+              {...tile}
+              key={`${tile.x}_${tile.y}_${tile.z}_${tile.value}`}
+              selected={
+                boardState.selected !== undefined &&
+                areEqualTiles(boardState.selected, tile)
+              }
+              hinted={isTileHinted(boardState, tile)}
+              onClick={() => onTileClick(tile)}
+            />
+          );
+        })}
+      </Bounds>
     </Canvas>
   );
 };

784d2cb972cac7e49e1c40cb0dc7b8e173748267
Maximilian Richter <richter.maximilian@gmx.net>
2023-02-22 15:15:08 +0100

    make github link clickable again

--- src/components/GithubBanner/GithubBanner.tsx
+++ src/components/GithubBanner/GithubBanner.tsx
@@ -3,6 +3,7 @@ import GithubCorner from "react-github-corner";
 export const GithubBanner: React.FC = () => {
   return (
     <GithubCorner
+      svgStyle={{ zIndex: 1 }}
       href="https://github.com/richterm/maxjongg"
       bannerColor="#2b223a"
       octoColor="#03edf9"

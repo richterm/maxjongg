0422a3986d9a1bf4cca5da51b4f9dde8bb9aced2
Maximilian Richter <richter.maximilian@gmx.net>
2023-05-12 20:50:53 +0200

    add minimal tile set

--- packages/shared/src/boards/tilesets.test.ts
+++ packages/shared/src/boards/tilesets.test.ts
@@ -1,9 +1,15 @@
 import { describe, test, expect } from "vitest";
-import { fullClassicTileSet } from "./tilesets";
+import { fullClassicTileSet, minimalClassicTileSet } from "./tilesets";
 import { alltileValuesMatchable } from "../board";
 
-describe("Classic Tile Set", () => {
+describe("Full Classic Tile Set", () => {
   test("integrity is given", () => {
     expect(alltileValuesMatchable(fullClassicTileSet).result).toEqual(true);
+  });
+});
+
+describe("Minimal Classic Tile Set", () => {
+  test("integrity is given", () => {
+    expect(alltileValuesMatchable(minimalClassicTileSet).result).toEqual(true);
   });
 });
--- packages/shared/src/boards/tilesets.ts
+++ packages/shared/src/boards/tilesets.ts
@@ -1,148 +1,236 @@
 import { TileValue } from "../types";
 
-export const fullClassicTileSet: Array<TileValue> = [
-  "Dots1",
-  "Dots1",
-  "Dots1",
-  "Dots1",
-  "Dots2",
-  "Dots2",
-  "Dots2",
-  "Dots2",
-  "Dots3",
-  "Dots3",
-  "Dots3",
-  "Dots3",
-  "Dots4",
-  "Dots4",
-  "Dots4",
-  "Dots4",
-  "Dots5",
-  "Dots5",
-  "Dots5",
-  "Dots5",
-  "Dots6",
-  "Dots6",
-  "Dots6",
-  "Dots6",
-  "Dots7",
-  "Dots7",
-  "Dots7",
-  "Dots7",
-  "Dots8",
-  "Dots8",
-  "Dots8",
-  "Dots8",
-  "Dots9",
-  "Dots9",
-  "Dots9",
-  "Dots9",
-  "Bamboo1",
-  "Bamboo1",
-  "Bamboo1",
-  "Bamboo1",
-  "Bamboo2",
-  "Bamboo2",
-  "Bamboo2",
-  "Bamboo2",
-  "Bamboo3",
-  "Bamboo3",
-  "Bamboo3",
-  "Bamboo3",
-  "Bamboo4",
-  "Bamboo4",
-  "Bamboo4",
-  "Bamboo4",
-  "Bamboo5",
-  "Bamboo5",
-  "Bamboo5",
-  "Bamboo5",
-  "Bamboo6",
-  "Bamboo6",
-  "Bamboo6",
-  "Bamboo6",
-  "Bamboo7",
-  "Bamboo7",
-  "Bamboo7",
-  "Bamboo7",
-  "Bamboo8",
-  "Bamboo8",
-  "Bamboo8",
-  "Bamboo8",
-  "Bamboo9",
-  "Bamboo9",
-  "Bamboo9",
-  "Bamboo9",
-  "Characters1",
-  "Characters1",
-  "Characters1",
-  "Characters1",
-  "Characters2",
-  "Characters2",
-  "Characters2",
-  "Characters2",
-  "Characters3",
-  "Characters3",
-  "Characters3",
-  "Characters3",
-  "Characters4",
-  "Characters4",
-  "Characters4",
-  "Characters4",
-  "Characters5",
-  "Characters5",
-  "Characters5",
-  "Characters5",
-  "Characters6",
-  "Characters6",
-  "Characters6",
-  "Characters6",
-  "Characters7",
-  "Characters7",
-  "Characters7",
-  "Characters7",
-  "Characters8",
-  "Characters8",
-  "Characters8",
-  "Characters8",
-  "Characters9",
-  "Characters9",
-  "Characters9",
-  "Characters9",
-  "DragonsGreen",
-  "DragonsGreen",
-  "DragonsGreen",
-  "DragonsGreen",
-  "DragonsRed",
-  "DragonsRed",
-  "DragonsRed",
-  "DragonsRed",
-  "DragonsWhite",
-  "DragonsWhite",
-  "DragonsWhite",
-  "DragonsWhite",
+const dots1: Array<TileValue> = ["Dots1", "Dots1"];
+const dots2: Array<TileValue> = ["Dots2", "Dots2"];
+const dots3: Array<TileValue> = ["Dots3", "Dots3"];
+const dots4: Array<TileValue> = ["Dots4", "Dots4"];
+const dots5: Array<TileValue> = ["Dots5", "Dots5"];
+const dots6: Array<TileValue> = ["Dots6", "Dots6"];
+const dots7: Array<TileValue> = ["Dots7", "Dots7"];
+const dots8: Array<TileValue> = ["Dots8", "Dots8"];
+const dots9: Array<TileValue> = ["Dots9", "Dots9"];
+
+const bamboo1: Array<TileValue> = ["Bamboo1", "Bamboo1"];
+const bamboo2: Array<TileValue> = ["Bamboo2", "Bamboo2"];
+const bamboo3: Array<TileValue> = ["Bamboo3", "Bamboo3"];
+const bamboo4: Array<TileValue> = ["Bamboo4", "Bamboo4"];
+const bamboo5: Array<TileValue> = ["Bamboo5", "Bamboo5"];
+const bamboo6: Array<TileValue> = ["Bamboo6", "Bamboo6"];
+const bamboo7: Array<TileValue> = ["Bamboo7", "Bamboo7"];
+const bamboo8: Array<TileValue> = ["Bamboo8", "Bamboo8"];
+const bamboo9: Array<TileValue> = ["Bamboo9", "Bamboo9"];
+
+const characters1: Array<TileValue> = ["Characters1", "Characters1"];
+const characters2: Array<TileValue> = ["Characters2", "Characters2"];
+const characters3: Array<TileValue> = ["Characters3", "Characters3"];
+const characters4: Array<TileValue> = ["Characters4", "Characters4"];
+const characters5: Array<TileValue> = ["Characters5", "Characters5"];
+const characters6: Array<TileValue> = ["Characters6", "Characters6"];
+const characters7: Array<TileValue> = ["Characters7", "Characters7"];
+const characters8: Array<TileValue> = ["Characters8", "Characters8"];
+const characters9: Array<TileValue> = ["Characters9", "Characters9"];
+
+const dragonsGreen: Array<TileValue> = ["DragonsGreen", "DragonsGreen"];
+const dragonsRed: Array<TileValue> = ["DragonsRed", "DragonsRed"];
+const dragonsWhite: Array<TileValue> = ["DragonsWhite", "DragonsWhite"];
+
+const flowers: Array<TileValue> = [
   "FlowersBamboo",
   "FlowersChrysanthemum",
   "FlowersOrchid",
   "FlowersPlum",
+];
+
+const seasons: Array<TileValue> = [
   "SeasonsAutumn",
   "SeasonsSpring",
   "SeasonsSummer",
   "SeasonsWinter",
-  "WindsEast",
-  "WindsEast",
-  "WindsEast",
-  "WindsEast",
-  "WindsNorth",
-  "WindsNorth",
-  "WindsNorth",
-  "WindsNorth",
-  "WindsSouth",
-  "WindsSouth",
-  "WindsSouth",
-  "WindsSouth",
-  "WindsWest",
-  "WindsWest",
-  "WindsWest",
-  "WindsWest",
+];
+
+const windsEast: Array<TileValue> = ["WindsEast", "WindsEast"];
+const windsNorth: Array<TileValue> = ["WindsNorth", "WindsNorth"];
+const windsSouth: Array<TileValue> = ["WindsSouth", "WindsSouth"];
+const windsWest: Array<TileValue> = ["WindsWest", "WindsWest"];
+
+export const minimalClassicTileSet: Array<TileValue> = [
+  ...dots1,
+
+  ...dots2,
+
+  ...dots3,
+
+  ...dots4,
+
+  ...dots5,
+
+  ...dots6,
+
+  ...dots7,
+
+  ...dots8,
+
+  ...dots9,
+
+  ...characters1,
+
+  ...characters2,
+
+  ...characters3,
+
+  ...characters4,
+
+  ...characters5,
+
+  ...characters6,
+
+  ...characters7,
+
+  ...characters8,
+
+  ...characters9,
+
+  ...bamboo1,
+
+  ...bamboo2,
+
+  ...bamboo3,
+
+  ...bamboo4,
+
+  ...bamboo5,
+
+  ...bamboo6,
+
+  ...bamboo7,
+
+  ...bamboo8,
+
+  ...bamboo9,
+
+  ...dragonsGreen,
+
+  ...dragonsRed,
+
+  ...dragonsWhite,
+
+  ...flowers,
+
+  ...seasons,
+
+  ...windsEast,
+
+  ...windsNorth,
+
+  ...windsSouth,
+
+  ...windsWest,
+];
+
+export const fullClassicTileSet: Array<TileValue> = [
+  ...dots1,
+  ...dots1,
+
+  ...dots2,
+  ...dots2,
+
+  ...dots3,
+  ...dots3,
+
+  ...dots4,
+  ...dots4,
+
+  ...dots5,
+  ...dots5,
+
+  ...dots6,
+  ...dots6,
+
+  ...dots7,
+  ...dots7,
+
+  ...dots8,
+  ...dots8,
+
+  ...dots9,
+  ...dots9,
+
+  ...bamboo1,
+  ...bamboo1,
+
+  ...bamboo2,
+  ...bamboo2,
+
+  ...bamboo3,
+  ...bamboo3,
+
+  ...bamboo4,
+  ...bamboo4,
+
+  ...bamboo5,
+  ...bamboo5,
+
+  ...bamboo6,
+  ...bamboo6,
+
+  ...bamboo7,
+  ...bamboo7,
+
+  ...bamboo8,
+  ...bamboo8,
+
+  ...bamboo9,
+  ...bamboo9,
+
+  ...characters1,
+  ...characters1,
+
+  ...characters2,
+  ...characters2,
+
+  ...characters3,
+  ...characters3,
+
+  ...characters4,
+  ...characters4,
+
+  ...characters5,
+  ...characters5,
+
+  ...characters6,
+  ...characters6,
+
+  ...characters7,
+  ...characters7,
+
+  ...characters8,
+  ...characters8,
+
+  ...characters9,
+  ...characters9,
+
+  ...dragonsGreen,
+  ...dragonsGreen,
+
+  ...dragonsRed,
+  ...dragonsRed,
+
+  ...dragonsWhite,
+  ...dragonsWhite,
+
+  ...flowers,
+
+  ...seasons,
+
+  ...windsEast,
+  ...windsEast,
+
+  ...windsNorth,
+  ...windsNorth,
+
+  ...windsSouth,
+  ...windsSouth,
+
+  ...windsWest,
+  ...windsWest,
 ];
